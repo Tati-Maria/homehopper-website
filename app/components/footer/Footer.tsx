85559f50f7979cc0b65d9d5906b28ec4c90ee1e3
Maria Tati <mauramaria92@gmail.com>
2023-04-01 19:36:02 +0100

    initial commit

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>© 2023 - HomeHopper All rights reserved</p>
+        <p>Created by <Link href={'https://github.com/Tati-Maria'}>Maria</Link> </p>
+    </footer>
+  )
+}
+
+export default Footer
